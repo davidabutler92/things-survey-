5b128c3aaa4ea6f6bf98287c987a76ebfd42370f
davidabutler92 <davidabutler92@yahoo.com>
2020-03-25 20:14:12 -0700

    add color wheel funtion

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event) {
+  event.preventDefault();
+
+var favoriteColor = $("#color").val();
+
+var flavor = $("input:radio[name=flavor]:checked").val();
+
+var flavor = $("input:radio[name=song]:checked").val();
+
+});
